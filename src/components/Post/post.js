af3647fb9477fdcb7c303bfd6bd522f9ea26982e
Chris Dunne <chrisdunne66@gmail.com>
2023-01-18 16:07:56 +0000

    0.8 -  Added delete post button

--- src/components/Post/post.js
+++ src/components/Post/post.js
@@ -1,9 +1,16 @@
 import React, {useState} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import DOMPurify from 'dompurify'
+import { updateArray } from '../../features/live-text'
 
 import './post.css'
 export default function Post({title, subtitle, body, type, id, timestamp}) {
+    const dispatch = useDispatch()
+    
     const [editMode, setEditMode] = useState(false)
+    const liveText = useSelector((state) => state.livetext.value)
+    // console.log(liveText)
+
     if(!title || !body ) return
 
     const readyPostTitle = title.replace('@', '')
@@ -21,6 +28,17 @@ export default function Post({title, subtitle, body, type, id, timestamp}) {
         setEditMode(true)
     }
 
+    const handleDelete = () =>{
+        console.log("Delete: " + id)
+        const filterCurrentPosts = liveText.filter(x => x.id !== id)
+        dispatch(updateArray(filterCurrentPosts))
+        localStorage.setItem("live-text", JSON.stringify(filterCurrentPosts))
+    }
+
+    const handleHide = () =>{
+        
+    }
+
     
 
 
@@ -33,6 +51,7 @@ export default function Post({title, subtitle, body, type, id, timestamp}) {
             <div className='post-item-time-stamp'>{timestamp}</div>
 
             {/* <button onClick={handleEdit}>Edit Me</button> */}
+            <button onClick={handleDelete}>Delete Me</button>
 
             <div className='post-item-headline-wrapper'>
 
