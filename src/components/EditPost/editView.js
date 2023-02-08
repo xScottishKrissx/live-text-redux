2d1f947631a1e18149001b2e8da3cb6c1f5974d6
Chris Dunne <chrisdunne66@gmail.com>
2023-02-08 13:41:36 +0000

    0.8.6 - You can now edit posts
    ...and save them using the save button. I need to close the edit mode on save but at least you can do the edit.

--- src/components/EditPost/editView.js
+++ src/components/EditPost/editView.js
@@ -1,21 +1,41 @@
 import { useState } from "react";
 import { useSelector, useDispatch } from 'react-redux'
-
+import { updateArray } from "../../features/live-text";
 // import EditTitle from "./editTitle";
 import EditPostField from "./editPostField";
 
 const EditTiptap = ({id, readyPostTitle, subtitle, body}) =>{
 
     const dispatch = useDispatch()
-    
+
+    const liveText = useSelector((state) => state.livetext.value)
+
     const [titleField, setTitle] = useState(readyPostTitle)
     const [subtitleField, setSubtitle] = useState(subtitle)
     const [bodyField, setBody] = useState(body)
 
     const saveEdit = () =>{
-        console.log(titleField)
-        console.log(subtitleField)
-        console.log(bodyField)
+        const getCurrentPost = liveText.filter(x => x.id === id)
+        console.log(getCurrentPost)
+
+        const updateCurrentPost = getCurrentPost.map(item =>{
+            if(item.id === id) return {...item, body:bodyField, title:titleField, subtitle:subtitleField}
+        })
+
+        const remainingPosts = liveText.filter(x => x.id !== id) 
+        const mergeObjects = [...remainingPosts, ...updateCurrentPost]
+        updateWebsite(mergeObjects)  
+
+        console.log(updateCurrentPost)
+
+        // console.log(titleField)
+        // console.log(subtitleField)
+        // console.log(bodyField)
+    }
+    
+    const updateWebsite = (newArray) =>{
+        dispatch(updateArray(newArray))
+        localStorage.setItem("live-text", JSON.stringify(newArray))   
     }
 
     return (
