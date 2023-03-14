ef2ac61e98fb0987a26355d9c5fec9d0dfa41c68
Chris Dunne <chrisdunne66@gmail.com>
2023-03-14 17:25:36 +0000

    0.13.2 - Added button that creates new live texts...
    ...and added ability to switch between active live texts

--- src/components/author-input.js
+++ src/components/author-input.js
@@ -4,17 +4,35 @@ import { setEdit } from '../features/editState'
 
 import EditTiptap from './EditPost/editView'
 import NewPost from './NewPost/NewPost'
+import {v4 as uuidv4} from 'uuid'
 
 import './author-input.css'
+
 
 export default function AuthorInput() {
   const dispatch = useDispatch()
   const editModeState = useSelector((state) => state.edit.value)
 
   const [showOverlay, setOverlay] = useState(true)
-
   const handleEdit = (x) => dispatch(setEdit({editing:x, editId: null}))
 
+  const liveTextMaster = JSON.parse(localStorage.getItem("liveTextMaster")) || []
+  const [liveTexts, setLiveTexts] = useState(liveTextMaster)
+  const [activeLiveText, setActiveLiveText] = useState()
+  const createNewLiveText = () => {
+    const newLiveText = {id:uuidv4(), content:[]}
+    setLiveTexts(liveTexts.concat(newLiveText))
+    localStorage.setItem("liveTextMaster", JSON.stringify(liveTexts))
+  }
+
+  const displayLiveTexts = liveTexts.map((x, index) => {
+    return (
+      <div key={index}>
+        {x.id} -- <button onClick={() => setActiveLiveText(x.id)}>View</button>
+      </div>
+    )
+  })
+  // localStorage.clear()
   return (
     <>
 
@@ -22,6 +40,7 @@ export default function AuthorInput() {
       <button onClick={()=>setOverlay(true)}>Display Control Panel</button>
       {showOverlay === true ? 
       <div className='confirm-post-overlay'> 
+        <button onClick={()=>localStorage.clear()}>Clear</button>
         <h1>Post Added</h1>
         
         <h3>Create New Post?</h3>
@@ -33,8 +52,14 @@ export default function AuthorInput() {
         <h3>Loggout</h3>
         <button>Logout</button>
 
-        <h4>Manage Live Text</h4>
-        <button>Manage</button>
+        <h4>Create Live Text</h4>
+        
+        <button onClick={createNewLiveText}>Create</button>
+        <h4>Active Live Text - {activeLiveText}</h4>
+
+        <h4>Manage Live Texts</h4>
+        {displayLiveTexts}
+
       </div>
     :
       <>
--- src/features/live-text.js
+++ src/features/live-text.js
@@ -3,6 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 const getLocalStorage =  JSON.parse(localStorage.getItem("live-text"))
 const defaultState = getLocalStorage ||  [] 
 
+console.log(defaultState)
+
+// const text = []
+// const thing1 = {"name" : "chris", "age":20, "job":"bricky"}
+// const thing2 = {"name" : "dave", "age":20, "job":"bricky"}
+// text.push(defaultState)
+// text.push(thing2)
+// console.log(text)
 export const liveTextSlice = createSlice({
     name:"live-text",
     initialState: {value:defaultState},
