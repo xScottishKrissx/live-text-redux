5a06cad17e70ea38ec4e4e06d90df6af60a61099
Chris Dunne <chrisdunne66@gmail.com>
2023-05-04 12:27:40 +0100

    0.26 - Improved appearance of elements when navigating with keyboard

--- src/components/ControlPanel/cpanelComponents/DisplayManageColumnsPanel/setColumnActive.js
+++ src/components/ControlPanel/cpanelComponents/DisplayManageColumnsPanel/setColumnActive.js
@@ -4,9 +4,9 @@ import { FaCheck, FaRegCircle } from 'react-icons/fa'
 export default function SetColumnActive({colIsActive, handleSetActive, columnId, columnContent}) {
   return (
     <div className='manageColumns-activeCheck'>{colIsActive ? 
-        <span className='checkActiveButton' title="Column is active" id="colIsActive"><FaCheck /></span> 
+        <button data-role="button" className='checkActiveButton' title="Column is active" id="colIsActive"><FaCheck /></button> 
         : 
-        <span className='checkActiveButton' title="Set Column Active" onClick={()=>handleSetActive(columnId, columnContent)}><FaRegCircle /></span>
+        <button data-role="button" className='checkActiveButton' title="Set Column Active" onClick={()=>handleSetActive(columnId, columnContent)}><FaRegCircle /></button>
         }
     </div>
   )
